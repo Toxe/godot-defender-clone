4d182cde51ec4d56cb517b42d48bdaa445a79f7a
Toxe <toxe@toxe.net>
2023-08-06 12:46:00 +0200

    Improve handling of horizontal player orientation

--- G01_Defender/scenes/player/player.gd
+++ G01_Defender/scenes/player/player.gd
@@ -7,14 +7,17 @@ const speed: float = 250
 
 func _physics_process(delta):
     var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
-    update_horizontal_sprite_flip(direction)
+    update_horizontal_orientation(direction.x)
     position += direction * speed * delta
 
 
-func update_horizontal_sprite_flip(direction: Vector2):
-    if not is_zero_approx(direction.x):
-        $Sprite.scale.x = 1 if direction.x > 0 else -1
+func get_horizontal_orientation() -> float:
+    return scale.x
+
+
+func update_horizontal_orientation(orientation: float):
+    if not is_zero_approx(orientation) and not is_equal_approx(orientation, get_horizontal_orientation()):
+        scale.x = 1.0 if orientation > 0.0 else -1.0
 
 
 func _on_area_entered(_area):
