9192a93c9491ca808891faf2479289a03a50ff28
Toxe <toxe@toxe.net>
2023-08-07 11:16:52 +0200

    Automatically destroy lasers that are off-screen

--- G01_Defender/scenes/player/laser.gd
+++ G01_Defender/scenes/player/laser.gd
@@ -16,3 +16,8 @@ func _physics_process(delta):
 
 func _on_area_entered(_area):
     queue_free()
+
+
+# automatically destroy when off-screen
+func _on_visible_on_screen_notifier_2d_screen_exited():
+    queue_free()
