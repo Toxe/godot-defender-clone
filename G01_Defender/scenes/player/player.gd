30f752c86a674f4d83cfce32d363fa26bedbeaec
Toxe <toxe@toxe.net>
2023-08-08 18:22:51 +0200

    Main level is build of 3 chunks; add player camera; level and enemies wrap around when player flies left or right

--- G01_Defender/scenes/player/player.gd
+++ G01_Defender/scenes/player/player.gd
@@ -32,5 +32,21 @@ func update_horizontal_orientation(orientation: float):
         scale.x = 1.0 if orientation > 0.0 else -1.0
 
 
+# wrap around to the other side of the level
+func wrap_around_level(node: Node2D):
+    if node.global_position.x < global_position.x:
+        node.position.x += 3*1152
+    else:
+        node.position.x -= 3*1152
+
+
 func _on_area_entered(_area):
     player_killed.emit()
+
+
+func _on_wrap_enemies_area_exited(area: Area2D):
+    wrap_around_level(area)
+
+
+func _on_wrap_level_chunks_area_exited(area: Area2D):
+    wrap_around_level(area)
