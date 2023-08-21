9284cf4925604fded454789740607f33cfdf984f
Toxe <toxe@toxe.net>
2023-08-21 11:23:54 +0200

    Add Pod enemy

---
+++ G01_Defender/scenes/enemies/pod.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const swarmer_scene = preload("swarmer.tscn")
+
+const number_of_swarmers := 3
+const swarmer_spawn_radius := 30.0
+const swarmer_spawn_direction_rotation := 30.0
+
+
+func swarmer_position() -> Vector2:
+    return global_position + Vector2.from_angle(randf_range(0, 2*PI)) * swarmer_spawn_radius
+
+
+func swarmer_direction() -> Vector2:
+    var direction = Vector2.RIGHT if $MovementComponent.velocity.x >= 0 else Vector2.LEFT
+    return direction.rotated(deg_to_rad(randf_range(-swarmer_spawn_direction_rotation, swarmer_spawn_direction_rotation)))
+
+
+# Pod destroyed, spawn a couple of Swarmers
+func _on_hitbox_component_destroyed():
+    for i in range(number_of_swarmers):
+        var swarmer = swarmer_scene.instantiate()
+        var swarmer_movement_component = swarmer.get_node("MovementComponent") as MovementComponent
+        swarmer.global_position = swarmer_position()
+        swarmer_movement_component.velocity = swarmer_direction() * swarmer_movement_component.velocity.length()
+        get_parent().call_deferred("add_child", swarmer)
