1909af33a1b578340f6dcac9be97e4ed92f38981
Toxe <toxe@toxe.net>
2023-08-21 13:40:40 +0200

    ShootingComponent emits a signal after it fired a shot

--- G01_Defender/scenes/components/shooting_component.gd
+++ G01_Defender/scenes/components/shooting_component.gd
@@ -1,5 +1,7 @@
 class_name ShootingComponent extends Node2D
 
+signal shot_fired
+
 @export var shot_scene: PackedScene = null
 @export var visible_on_screen_notifier: VisibleOnScreenNotifier2D = null
 @export var min_shot_delay := 1.0
@@ -25,6 +27,7 @@ func shoot():
     shot.setup(global_position)
     get_parent().get_parent().add_child(shot)
     start_shooting()
+    shot_fired.emit()
 
 
 func _on_timer_timeout():
