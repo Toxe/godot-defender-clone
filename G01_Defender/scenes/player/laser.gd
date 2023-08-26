1216da6a6c7339b02f9f195fe86fdd08841f5dad
Toxe <toxe@toxe.net>
2023-08-26 14:12:21 +0200

    Laser doesn't need a script anymore

--- G01_Defender/scenes/player/laser.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func setup(spawn_position: Vector2, orientation: float):
-    global_position = spawn_position
-    $MovementComponent.direction = Vector2.RIGHT * orientation
-    scale.x = orientation
