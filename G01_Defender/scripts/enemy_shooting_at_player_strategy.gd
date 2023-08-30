7f28dec392493e99d967d2fa99b26f929ed0b2d1
Toxe <toxe@toxe.net>
2023-08-30 14:41:57 +0200

    Code cleanup

--- G01_Defender/scripts/enemy_shooting_at_player_strategy.gd
+++ G01_Defender/scripts/enemy_shooting_at_player_strategy.gd
@@ -5,9 +5,8 @@ class_name EnemyShootingAtPlayerStrategy extends Resource
 
 # Note: shot is not yet in the tree so shot.global_position is still (0, 0) at this point and shot.get_tree() would return null
 func update(scene_tree: SceneTree, shot: Node2D, shot_global_position: Vector2):
-    var player = scene_tree.get_first_node_in_group("player") as Node2D
-
     if shot.has_node("MovementComponent"):
-        var shot_movement_component = shot.get_node("MovementComponent") as MovementComponent
+        var player = scene_tree.get_first_node_in_group("player") as Node2D
         var direction = player.global_position - shot_global_position
+        var shot_movement_component = shot.get_node("MovementComponent") as MovementComponent
         shot_movement_component.direction = direction.rotated(deg_to_rad(randf_range(-spread_angle / 2.0, spread_angle / 2.0)))
