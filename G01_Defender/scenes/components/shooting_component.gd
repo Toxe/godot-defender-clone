2272207eafa791602ffeb647e401b42d047dbb17
Toxe <toxe@toxe.net>
2023-08-30 10:50:13 +0200

    Enemies use EnemyShootingAtPlayerAIComponent to shoot (roughly) in the direction of the player

--- G01_Defender/scenes/components/shooting_component.gd
+++ G01_Defender/scenes/components/shooting_component.gd
@@ -4,6 +4,7 @@ signal shot_fired
 
 @export var shot_scene: PackedScene = null
 @export var visible_on_screen_notifier: VisibleOnScreenNotifier2D = null
+@export var shooting_ai_component: Node = null
 @export var min_shot_delay := 1.0
 @export var max_shot_delay := 2.0
 @export var one_shot := false
@@ -31,6 +32,9 @@ func shoot(direction: Vector2 = Vector2.ZERO) -> Node2D:
     if shot.has_node("MovementComponent"):
         var shot_movement_component = shot.get_node("MovementComponent") as MovementComponent
         shot_movement_component.direction = direction
+
+    if shooting_ai_component:
+        shooting_ai_component.update(shot, global_position)  # note: shot is not yet in the tree so shot.global_position is still (0,0) at this point
 
     get_parent().get_parent().add_child(shot)
     start_shooting()
--- G01_Defender/scenes/enemies/bomb.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-
-func setup(spawn_position: Vector2):
-    global_position = spawn_position
