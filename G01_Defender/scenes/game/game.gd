2c039578292bbf1d755762b4034a2bb71dbe40a4
Toxe <toxe@toxe.net>
2023-08-31 11:42:07 +0200

    Move window title updates to Game controller

--- G01_Defender/scenes/game/game.gd
+++ G01_Defender/scenes/game/game.gd
@@ -2,9 +2,18 @@ class_name Game extends Node
 
 
 func _ready():
+    update_window_title()
     load_level("main")
 
 
 func load_level(level_name: String):
     var level = load("res://scenes/levels/%s.tscn" % level_name).instantiate() as Node
     add_child(level)
+
+
+func update_window_title():
+    get_window().title = "Godot Defender Clone    [milestone 4, %d FPS]" % [Performance.get_monitor(Performance.TIME_FPS)]
+
+
+func _on_update_window_title_timer_timeout():
+    update_window_title()
--- G01_Defender/scenes/levels/main.gd
+++ G01_Defender/scenes/levels/main.gd
@@ -1,17 +1,5 @@
 extends Node
 
 
-func _ready():
-    update_window_title()
-
-
-func update_window_title():
-    get_window().title = "Godot Defender Clone    [milestone 4, %d FPS]" % [Performance.get_monitor(Performance.TIME_FPS)]
-
-
 func _on_player_player_killed():
     get_tree().reload_current_scene()
-
-
-func _on_update_window_title_timer_timeout():
-    update_window_title()
