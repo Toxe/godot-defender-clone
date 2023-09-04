bb6b97952e1dbd3587c78f664dd4131817f84c98
Toxe <toxe@toxe.net>
2023-09-04 17:47:04 +0200

    Add function to disable HitboxComponent hit detection

--- G01_Defender/scenes/components/hitbox_component.gd
+++ G01_Defender/scenes/components/hitbox_component.gd
@@ -3,6 +3,10 @@ class_name HitboxComponent extends Area2D
 signal destroyed()
 
 
+func disable():
+    set_deferred("process_mode", PROCESS_MODE_DISABLED)
+
+
 func _on_area_entered(_area):
     destroyed.emit()
     get_parent().queue_free()
