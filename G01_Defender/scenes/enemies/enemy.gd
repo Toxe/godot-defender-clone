8d79e6ddd7f8dbbd75ca3ea19df28c7ac00aaa06
Toxe <toxe@toxe.net>
2023-09-06 12:04:58 +0200

    Enemies emit "enemy_destroyed" signal when destroyed

--- G01_Defender/scenes/enemies/enemy.gd
+++ G01_Defender/scenes/enemies/enemy.gd
@@ -1,3 +1,16 @@
 class_name Enemy extends Node2D
 
 @export var score := 0
+
+
+func _ready():
+    # automatically connect to the HitboxComponent "destroyed" signal
+    for child in get_children():
+        var hitbox_component = child as HitboxComponent
+        if hitbox_component:
+            hitbox_component.destroyed.connect(_on_destroyed)
+            break
+
+
+func _on_destroyed():
+    Events.enemy_destroyed.emit(self)
