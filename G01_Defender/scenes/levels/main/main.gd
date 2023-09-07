cfab9a68c468622e0bc30ad8b43712714358dd5f
Toxe <toxe@toxe.net>
2023-09-07 12:22:57 +0200

    Rename the score label to ScoreCounter

--- G01_Defender/scenes/levels/main/main.gd
+++ G01_Defender/scenes/levels/main/main.gd
@@ -7,4 +7,4 @@ func _on_player_player_killed():
     # show game over text, wait for a moment and then finish the game
     $UI/GameOverLabel.visible = true
     await get_tree().create_timer(1.5).timeout
-    game_finished.emit($UI/ScoreLabel.score)
+    game_finished.emit($UI/ScoreCounter.score)
