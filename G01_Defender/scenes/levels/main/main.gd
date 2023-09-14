f0b9f9b1d837c78e2e197a5ac3fb2db093f666aa
Toxe <toxe@toxe.net>
2023-09-14 13:02:37 +0200

    Replace score counter UI label with ScoreCounter component and a simple label

--- G01_Defender/scenes/levels/main/main.gd
+++ G01_Defender/scenes/levels/main/main.gd
@@ -5,4 +5,4 @@ func _on_player_player_killed():
     # show game over text, wait for a moment and then finish the game
     $UI/GameOverLabel.visible = true
     await get_tree().create_timer(1.5).timeout
-    Events.game_finished.emit($UI/ScoreCounter.score)
+    Events.game_finished.emit($ScoreCounter.score)
---
+++ G01_Defender/scenes/levels/main/score_counter.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var score_label: Label = null
+
+var score = 0
+
+
+func _ready():
+    Events.enemy_destroyed.connect(_on_enemy_destroyed)
+    update_label()
+
+
+func update_label():
+    if score_label:
+        score_label.text = "Score: " + str(score)
+
+
+func _on_enemy_destroyed(enemy: Enemy):
+     score += enemy.score
+     update_label()
