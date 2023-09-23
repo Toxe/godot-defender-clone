7c42d435a55e7fa8c16bc1c2c605c3064ce9c8a4
Toxe <toxe@toxe.net>
2023-09-23 12:43:29 +0200

    EnemyShootingAtPlayerStrategy: If the player is dead shoot into a random direction

--- G01_Defender/scripts/enemy_shooting_at_player_strategy.gd
+++ G01_Defender/scripts/enemy_shooting_at_player_strategy.gd
@@ -6,7 +6,11 @@ class_name EnemyShootingAtPlayerStrategy extends Resource
 # Note: shot is not yet in the tree so shot.global_position is still (0, 0) at this point and shot.get_tree() would return null
 func update(scene_tree: SceneTree, shot: Node2D, shot_global_position: Vector2):
     if shot.has_node("MovementComponent"):
-        var player = scene_tree.get_first_node_in_group("player") as Node2D
-        var direction = player.global_position - shot_global_position
         var shot_movement_component = shot.get_node("MovementComponent") as MovementComponent
-        shot_movement_component.direction = direction.rotated(deg_to_rad(randf_range(-spread_angle / 2.0, spread_angle / 2.0)))
+        var player = scene_tree.get_first_node_in_group("player") as Node2D
+        if player:
+            # shoot roughly into the direction of the player
+            shot_movement_component.direction = (player.global_position - shot_global_position).rotated(deg_to_rad(randf_range(-spread_angle / 2.0, spread_angle / 2.0)))
+        else:
+            # no player found (could be dead), so just shoot into a random direction
+            shot_movement_component.direction = Vector2.RIGHT.rotated(randf_range(0.0, 2.0 * PI))
