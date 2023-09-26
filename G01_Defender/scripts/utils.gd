3353156ba72cbc460b17fd4fdc7bac8f8bb14cee
Toxe <toxe@toxe.net>
2023-09-26 11:18:40 +0200

    Change text parameter to Variant

--- G01_Defender/scripts/utils.gd
+++ G01_Defender/scripts/utils.gd
@@ -1,9 +1,9 @@
 class_name Utils
 
 
-static func debug_log(object: Object = null, text: String = ""):
+static func debug_log(object: Object = null, text: Variant = null):
     var object_id := "" if object == null else str(object) + " | "
-    var append_text := "" if text == "" else " | " + text
+    var append_text := "" if text == null else " | " + str(text)
     print("[%d | %d] %s%s:%d%s" % [Engine.get_physics_frames(), Engine.get_process_frames(), object_id, get_stack()[1]["function"], get_stack()[1]["line"], append_text])
 
 
