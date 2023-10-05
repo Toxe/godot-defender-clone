4a41b734cd93a6e70fe7f32d8e0e66e70cfc455f
Toxe <toxe@toxe.net>
2023-10-05 19:55:54 +0200

    Player earns an extra life for every 10000 score points

--- G01_Defender/scenes/levels/main/lives_counter.gd
+++ G01_Defender/scenes/levels/main/lives_counter.gd
@@ -6,11 +6,17 @@ var _lives = 2
 
 
 func _ready():
+    Events.extra_life.connect(_on_extra_life)
     update_label()
 
 
 func has_lives_left() -> bool:
     return _lives > 0
+
+
+func increase():
+    _lives += 1
+    update_label()
 
 
 func decrease():
@@ -22,3 +28,7 @@ func decrease():
 func update_label():
     if lives_label:
         lives_label.text = "Lives: " + str(_lives)
+
+
+func _on_extra_life():
+    increase()
--- G01_Defender/scenes/levels/main/score_counter.gd
+++ G01_Defender/scenes/levels/main/score_counter.gd
@@ -4,6 +4,8 @@ extends Node
 
 var score = 0
 
+var _score_until_next_extra_life := 10000
+
 
 func _ready():
     Events.enemy_destroyed.connect(_on_enemy_destroyed)
@@ -18,3 +20,8 @@ func update_label():
 func _on_enemy_destroyed(enemy: Enemy):
     score += enemy.score
     update_label()
+
+    _score_until_next_extra_life -= enemy.score
+    if _score_until_next_extra_life <= 0:
+        _score_until_next_extra_life += 10000
+        Events.extra_life.emit()
